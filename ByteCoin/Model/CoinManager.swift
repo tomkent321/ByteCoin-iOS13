7e59f8ebc729403fedc2cfc7732342f83508e655
Tom K <tomkent321@yahoo.com>
2021-02-05 21:36:22 -0700

    initial commit

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -15,5 +15,5 @@ struct CoinManager {
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
-    
+    //git check
 }
